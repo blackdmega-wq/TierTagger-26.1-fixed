496dd6878ed5bcdbc5ca6c2e3a63a6bd18bc3aa8
Outversal <blackdmega@gmail.com>
2026-05-24 20:44:58 +0200

    fix: update Fabric Loader to 0.19.2 and Fabric API to 0.145.1+26.1 for MC 26.1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,8 +1,8 @@
 object BuildConfig {
     const val MINECRAFT_VERSION: String = "26.1"
-    const val FABRIC_LOADER_VERSION: String = "0.18.4"
+    const val FABRIC_LOADER_VERSION: String = "0.19.2"
     const val NEOFORGE_VERSION: String = "26.1.0.1-beta"
-    const val FABRIC_API_VERSION: String = "0.144.3+26.1"
+    const val FABRIC_API_VERSION: String = "0.145.1+26.1"
     const val UKULIB_VERSION: String = "2.0.0+26.1"
 
     const val MOD_VERSION: String = "2.5.1"
